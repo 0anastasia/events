1e83380d74c13d1483c5be69f820c7b190d4da05
Anastasia <anastasia.chashch@mail.ru>
2023-07-25 21:50:53 +0500

    done

--- src/js/goblin.js
+++ src/js/goblin.js
@@ -1,17 +1,17 @@
 export default class Goblin {
-    constructor() {
-      this.currentCell = -1;
-    }
-  
-    setCurrentCell(index) {
-      this.currentCell = index;
-    }
-  
-    getCurrentCell() {
-      return this.currentCell;
-    }
-  
-    hit(index) {
-      return this.currentCell === index;
-    }
+  constructor() {
+    this.currentCell = -1;
   }
+
+  setCurrentCell(index) {
+    this.currentCell = index;
+  }
+
+  getCurrentCell() {
+    return this.currentCell;
+  }
+
+  hit(index) {
+    return this.currentCell === index;
+  }
+}
